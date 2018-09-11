63734e6cfc339e7edc0bde331574ad9e4986c08f
Josh <josh.dowdle@prioritydispatch.net>
2018-09-11 17:08:14 -0600

    Add a test netlify form for trouble shooting. again...

--- src/pages/about-us.js
+++ src/pages/about-us.js
@@ -46,6 +46,38 @@ export const AboutUs = () => (
           voluptate tempora fugit, tenetur nam. Dolorem iste voluptas magnam
           delectus perspiciatis in iusto, commodi inventore a!
         </p>
+
+        <div>
+          <form name="contact" method="POST" netlify>
+            <p>
+              <label>
+                Your Name: <input type="text" name="name" />
+              </label>
+            </p>
+            <p>
+              <label>
+                Your Email: <input type="email" name="email" />
+              </label>
+            </p>
+            <p>
+              <label>
+                Your Role:{' '}
+                <select name="role[]" multiple>
+                  <option value="leader">Leader</option>
+                  <option value="follower">Follower</option>
+                </select>
+              </label>
+            </p>
+            <p>
+              <label>
+                Message: <textarea name="message" />
+              </label>
+            </p>
+            <p>
+              <button type="submit">Send</button>
+            </p>
+          </form>
+        </div>
       </Box>
     </Container>
   </Layout>
