616f80aa8229135d179a877dfee6746a1e0ebb73
Josh <josh.dowdle@prioritydispatch.net>
2018-09-12 07:19:28 -0600

    Remove gatsby offline plugin to attempt form submit.

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,6 +16,6 @@ module.exports = {
         icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
-    'gatsby-plugin-offline',
+    // 'gatsby-plugin-offline',
   ],
 }
